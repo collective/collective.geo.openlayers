2d5ddd842dbafed1605cdbdda56d6303a83c61f1
David\ Beitey <david@davidjb.com>
2012-05-23 14:18:48 +1000

    Changing to README.rst for GitHub

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.5.dev0'
 setup(name='collective.geo.openlayers',
       version=version,
       description="openlayers support for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
